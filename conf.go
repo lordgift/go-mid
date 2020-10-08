5b80ba48aff872e6b3d9c6fc31df4d0f6314e777
LordGift <jarupath.j@cdg.co.th>
2020-10-08 09:03:18 +0700

    merged feature/mq

--- conf.go
+++ conf.go
@@ -1,8 +1,38 @@
 package main
 
-import "go-mid/mq"
+import (
+	"fmt"
+	"go-mid/mq"
+	"time"
+)
 
 func main() {
-	mq.Publish()
+
+	//go loop("first")
+	//go loop("second")
+
+	higherOrder(func(isOk bool) {
+		if isOk {
+			fmt.Print("99999")
+		}
+	})
+
+	//s := services.CreateService()
+	//r := services.CreateRouter(s)
+	//r.Run()
+
 	mq.Consume()
+	mq.Publish()
+}
+
+func higherOrder(completion func(bool)) {
+	completion(true)
+}
+
+func loop(my string) {
+	for i := 1; i < 20; i++ {
+		fmt.Printf("%s:%d\n", my, i)
+		time.Sleep(1 * time.Second)
+	}
+
 }
